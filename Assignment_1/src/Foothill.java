df1112eeb29faffdb3e58f07e8c07b17a45b7090
shabnam60 <Shabnam.Rouzbeh@litescape.com>
2016-06-23 14:24:13 -0700

    assignment1

---
+++ Assignment_1/src/Foothill.java
@@ -0,0 +1,9 @@
+
+public class Foothill {
+	 public static void main (String [] args)
+	 {
+		 System.out.println ("Hello Professor Loceff!");
+		 System.out.println ("My last name is: Rouzbeh");
+	 }
+
+}
